150e8172a284c4d7b54d36ef6244d0f9119db8f9
bobnearents <bobnearents@yahoo.com>
2019-02-27 12:23:48 -0800

    finished testing

--- test/bookmark-endpoints.spec.js
+++ test/bookmark-endpoints.spec.js
@@ -29,24 +29,28 @@ describe('Bookmark Endpoints', () => {
     }
   ];
 
-  before(() => {
+  before('establish connection to database',() => {
     db = knex({
       client: 'pg',
       connection: process.env.DB_URL_TEST
     });
+    app.set('db', db);
   });
-  before(() => db('bookmarks').truncate());
-  afterEach(() => db('bookmarks').truncate());
-  after(() => db.destroy());
+  before('clean the table',() => db('bookmarks').truncate());
+  afterEach('maid service', () => db('bookmarks').truncate());
+  after('end connection', () => db.destroy());
 
   context(`Given 'bookmarks' has data`, () => {
     beforeEach(() => {
       return db.into('bookmarks').insert(testBookmarks);
     });
     it(`'getAllBookmarks()' resolves all bookmarks from 'bookmarks' table`, () => {
-      return BookmarksService.getAllBookmarks(db).then(actual => {
-        expect(actual).to.eql(testBookmarks);
-      });
+      // return BookmarksService.getAllBookmarks(db).then(actual => {
+      //   expect(actual).to.eql(testBookmarks);
+      // });
+      return supertest(app)
+        .get('/bookmarks')
+        .expect(200, testBookmarks)
     });
     it(`'getBookmarkById()' resolves an article by ID from 'bookmarks' table`, () => {
       const id = 3;
@@ -67,6 +71,31 @@ describe('Bookmark Endpoints', () => {
           ...newBookmarkFields
         });
       });
+    });
+    it(`'deleteBookmark' resolves with a deleted article by ID from 'bookmarks' table`, () => {
+      const id = 1;
+      const updatedBookmarks = testBookmarks.filter(bookmark => bookmark.id !== id);
+      return BookmarksService.deleteBookmark(db, id)
+        .then(() => BookmarksService.getAllBookmarks(db))
+        .then((allBookmarks) => {
+          expect(allBookmarks).to.eql(updatedBookmarks)
+        });
+    });
+  });
+  context('given bookmarks has no data', () => {
+    it(`'getAllBookmarks' resolves an empty array from 'bookmarks' table`, () => {
+      return BookmarksService.getAllBookmarks(db)
+        .then((actual) => {expect(actual).to.eql([])});
+    });
+    it(`'insertBookmark' resolves with an updated array of bookmarks`, () => {
+      const newBookmark = testBookmarks[0];
+      return BookmarksService.insertBookmark(db, newBookmark)
+        .then(() => {
+          return BookmarksService.getAllBookmarks(db)
+        })
+        .then((allBookmarks) => {
+          expect(allBookmarks).lengthOf(1);
+        });
     });
   });
 });
